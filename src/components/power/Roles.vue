1532ad627cb113ed0fde6ca5841beae7d8647940
yufengjiaoyu <1147305508@qq.com>
2021-10-02 11:48:31 +0800

    完成角色添加

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -10,7 +10,7 @@
         <!-- 添加角色列表区 -->
         <el-row>
             <el-col>
-                  <el-button type="primary">添加角色</el-button>
+                  <el-button type="primary" @click="addRoleDialogVisible = true">添加角色</el-button>
             </el-col>
         </el-row>
         <!-- 角色列表区 -->
@@ -30,6 +30,26 @@
           </el-table-column>
         </el-table>
     </el-card>
+<!-- 添加角色弹框 -->
+    <el-dialog
+      title="添加角色"
+      :visible.sync="addRoleDialogVisible"
+      width="50%">
+      <span>
+        <el-form :model="roleForm" :rules="roleRules" ref="addRoleRef" label-width="100px" >
+        <el-form-item label="角色名称" prop="roleName">
+        <el-input v-model="roleForm.roleName" ></el-input>
+         </el-form-item>                   
+         <el-form-item label="角色描述" prop="roleDesc">
+         <el-input v-model="roleForm.roleDesc"></el-input>
+        </el-form-item>
+        </el-form>
+      </span>
+     <span slot="footer" >
+    <el-button @click= "addRoleDialogVisible = false">取 消</el-button>
+    <el-button type="primary"  @click="addRoleInfo">确 定</el-button>
+  </span>
+</el-dialog>
   </div>
 </template>
 
@@ -41,10 +61,32 @@ export default {
   data() {
     return {
     // 所有角色列表数据
+      roleForm:{
+        roleName: '',
+        roleDesc: ''
+    },
+      roleRules: {roleDesc: [{ required: true, message: '请输入角色描述',  trigger:'blur' }],
+                 roleName: [{ required: true, message: '请输入角色名称',   trigger:'blur'}]
+                 
+                 },
+      addRoleDialogVisible:false,
       rolelist: []
     }
   },
   methods: {
+    addRoleInfo(){
+    this.$refs.addRoleRef.validate(async valid => {
+     if(!valid) return 
+     const {data: res} = await this.$http.post('roles',this.roleForm)
+     if ( res!==201 ) {
+       this.$message.error('添加角色失败')
+     }
+      this.$message.success('添加角色成功')
+     this.addRoleDialogVisible=false
+     this.getRolesList()
+
+    })
+    },
     async getRolesList() {
       const { data: res } = await this.$http.get('roles')
       if (res.meta.status !== 200) {
